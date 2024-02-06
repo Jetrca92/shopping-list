857aa593d06f9d7c38e1e468d53c6838bf8a8927
Jetrca92 <blaz.guzej@gmail.com>
2024-02-06 21:15:13 +0100

    Complete app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import './main.css'
 
 const header = ref('Shopping List App')
+const characterCount = computed(() => {
+	return newItem.value.length
+})
 const items = ref([
-	{id: 1, label: "10 party hats"},
-	{id: 2, label: "2 board games"},
-	{id: 3, label: "20 cups"},
+	{id: 1, label: "10 party hats", purchased: true, highPriority: true},
+	{id: 2, label: "2 board games", purchased: false, highPriority: false},
+	{id: 3, label: "20 cups", purchased: false, highPriority: false},
 ])
 const newItem = ref("")
 const newItemHighPriority = ref(false)
 const saveItem = () => {
-	items.value.push({id: items.value.length + 1, label: newItem.value})
+	items.value.push({id: items.value.length + 1, label: newItem.value, highPriority: newItemHighPriority.value})
 	newItem.value = ""
+	newItemHighPriority.value = false
 }
 const editing = ref(false)
 const doEdit = (e) => {
 	editing.value = e
 	newItem.value = ""
+}
+const togglePurchased = (item) => {
+	item.purchased = !item.purchased
 }
 </script>
 
@@ -47,13 +54,25 @@ const doEdit = (e) => {
 				High Priority
 			</label>
 			<button 
+				:disabled="newItem.length === 0"
 				class="btn btn-primary"
 			>
 				Save item
 			</button>
 		</form>
+		<p class="counter" v-if="editing">
+				{{ characterCount }}/200
+			</p>
 		<ul>
-			<li v-for="item in items" :key="item.id">
+			<li 
+				v-for="item in items" 
+				:key="item.id"
+				:class="{ 
+					strikeout: item.purchased,
+					priority: item.highPriority
+				}"
+				@click="togglePurchased(item)"
+			>
 				{{ item.label }}
 			</li>
 		</ul>
